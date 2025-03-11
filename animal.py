4796719e6d31e0848e525c517cbc9768588fae8a
1161239811@qq.com <sjy@DESKTOP-4LVV8CU.>
2025-03-11 21:35:58 +0800

    add animal.py

--- animal.py
+++ animal.py
@@ -1,9 +1,14 @@
 import sys
 
+def cat():
+    print('miao')
 def default():
     print('Hello')
-
+    
 def main():
+    if sys.argv[1]=='cat':
+        cat()
+    else:
       default()
 if __name__=='__main__':
     main()
