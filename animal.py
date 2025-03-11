f4948ee38d1c89b731f448a6dc1c20baa4162443
1161239811@qq.com <sjy@DESKTOP-4LVV8CU.>
2025-03-11 21:43:45 +0800

    add dog

--- animal.py
+++ animal.py
@@ -1,9 +1,15 @@
 import sys
 
+def dog():
+    print('woof')
+
 def default():
     print('Hello')
 
 def main():
+    if sys.argv[1]=='dog':
+        dog()
+    else:
       default()
 if __name__=='__main__':
     main()
